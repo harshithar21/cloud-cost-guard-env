9c97cf878b22f236aaede9bbc3827afa8929cac9
CloudCostGuard Team <team@cloudcostguard.io>
2026-04-08 21:52:59 +0530

    changes2

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def greet_json():
     return {"Hello": "World!"}
+
+def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    main()
